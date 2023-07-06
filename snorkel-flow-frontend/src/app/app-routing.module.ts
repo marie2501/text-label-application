3225cb707eb8047cefae8d70a00802d1a8bd5708
Marie Braun <mariebraun@MacBook-Pro-von-Marie.local>
2023-07-07 00:12:11 +0200

    Frontend: add public components

--- snorkel-flow-frontend/src/app/app-routing.module.ts
+++ snorkel-flow-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./components/public/homepage/homepage.component";
+import {LoginComponent} from "./components/public/auth/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- snorkel-flow-frontend/src/app/app.module.ts
+++ snorkel-flow-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/public/homepage/homepage.component';
+import { NavbarComponent } from './components/public/navbar/navbar.component';
+import { LoginComponent } from './components/public/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
