0a360c264265b6b0b926950dd72bf8feb6ae63f9
Marie Braun <marie.braun@uni-duesseldorf.de>
2023-07-12 01:39:47 +0200

    Backend: add Workflow Modell

--- snorkel_flow_backend/snorkel_flow_backend/settings.py
+++ snorkel_flow_backend/snorkel_flow_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_cleanup.apps.CleanupConfig',
     'corsheaders',
+    'workflow_settings',
 ]
 
 MIDDLEWARE = [
--- snorkel_flow_backend/workflow_settings/admin.py
+++ snorkel_flow_backend/workflow_settings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from workflow_settings.models import Workflow
+
+
 # Register your models here.
+@admin.register(Workflow)
+class WorkflowAdmin(admin.ModelAdmin):
+    pass
