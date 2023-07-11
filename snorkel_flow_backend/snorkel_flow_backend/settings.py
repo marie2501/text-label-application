c839b55289011a01e3bdc4d002480aa6ec1efabc
Marie Braun <marie.braun@uni-duesseldorf.de>
2023-07-11 22:17:17 +0200

    Backend: update settings, urls from imported packages

--- snorkel_flow_backend/snorkel_flow_backend/settings.py
+++ snorkel_flow_backend/snorkel_flow_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +130,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS Settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
 
 CORS_ALLOW_ALL_ORIGINS = False
 
--- snorkel_flow_backend/snorkel_flow_backend/urls.py
+++ snorkel_flow_backend/snorkel_flow_backend/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^authentication/', include('the_social_network.urls.authenticationUrls')),
+    url(r'^accounts/', include('the_social_network.urls.accountUrls')),
+    url(r'^search/', include('the_social_network.urls.searchUrls')),
+    url(r'^contents/', include('the_social_network.urls.contentUrls')),
 ]
