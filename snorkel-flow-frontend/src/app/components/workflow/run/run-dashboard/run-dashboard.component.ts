e9b394bc0647415359bc70280224ed8841466e98
mariebraun <marie.braun@uni-duesseldorf.de>
2023-10-18 01:08:47 +0200

    Frontend: dashboard for runs

--- snorkel-flow-frontend/src/app/components/workflow/run/run-dashboard/run-dashboard.component.ts
+++ snorkel-flow-frontend/src/app/components/workflow/run/run-dashboard/run-dashboard.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RunService} from "../../../../services/workflow/run.service";
+import {LabelfunctionService} from "../../../../services/workflow/labelfunction.service";
+import {ActivatedRoute} from "@angular/router";
+import {RunModel} from "../../../../models/run.model";
+import {LabelfunctionModel} from "../../../../models/labelfunction.model";
 
 @Component({
   selector: 'app-run-dashboard',
   templateUrl: './run-dashboard.component.html',
   styleUrls: ['./run-dashboard.component.css']
 })
-export class RunDashboardComponent {
+export class RunDashboardComponent implements OnInit{
+  workflow_id: number = 0;
+  run_id: number = 0;
+  // @ts-ignore
+  run: RunModel;
+  runLoaded!: Promise<boolean>;
+  labelfunctions: LabelfunctionModel[] = [];
 
+
+  constructor(private runService: RunService, private labelfunctionService: LabelfunctionService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.workflow_id = this.route.snapshot.params['id'];
+    this.run_id = this.route.snapshot.params['runID'];
+    this.runService.getlabelfunctionRun(this.run_id).subscribe(respData => {
+      console.log(respData);
+      this.run = respData;
+      this.labelfunctions = this.run.labelfunctions!;
+      this.runLoaded = Promise.resolve(true);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  executeRun() {
+    this.runService.executelabelfunctionRun(this.run_id).subscribe(respData => {
+      console.log(respData);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
