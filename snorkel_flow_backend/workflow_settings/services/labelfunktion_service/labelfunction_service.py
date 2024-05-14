f1d60e6a1b1bf2a363b865c97ebd958a551a41b3
mariebraun <marie.braun@uni-duesseldorf.de>
2024-05-15 00:42:38 +0200

    check labels for correctness

--- snorkel_flow_backend/workflow_settings/services/labelfunktion_service/labelfunction_service.py
+++ snorkel_flow_backend/workflow_settings/services/labelfunktion_service/labelfunction_service.py
@@ -9,7 +9,7 @@ from zen_queries import fetch, queries_disabled
 from rest_framework import status
 
 from snorkel_flow_backend.settings import MEDIA_ROOT
-from workflow_settings.models import Labelfunction, File, Run
+from workflow_settings.models import Labelfunction, File, Run, Workflow
 from workflow_settings.serializers.serializers_labelfunction import LabelfunctionSerializer, LabelfunctionCreateSerializer
 
 class LabelfunctionService:
@@ -26,6 +26,21 @@ class LabelfunctionService:
             serialziers_label = LabelfunctionSerializer(labelfunction[0])
             return status.HTTP_200_OK, serialziers_label.data
         return status.HTTP_404_NOT_FOUND, {"message": "Labels don't exists"}
+
+    def __checkLabels(self, workflow_id, L_train_list, L_unlabeled_list):
+        labelfunction_filter = Labelfunction.objects.filter(workflow_id=workflow_id, type='labels')
+        if labelfunction_filter.exists():
+            labelfunction_object_code = labelfunction_filter[0].code
+            labels_split = labelfunction_object_code.split()
+            labels_split.remove('=')
+            while '=' in labels_split:
+                labels_split.remove('=')
+            L_train_list.extend(L_unlabeled_list)
+
+            for el in L_train_list:
+                if str(el) not in labels_split:
+                    raise ValueError(f'The label { el } was not spezified by the workflow creator')
+
 
     def compile_labelfunction(self, workflow_id, code):
 
@@ -54,34 +69,42 @@ class LabelfunctionService:
 
             imports = Labelfunction.objects.filter(workflow_id=workflow_id, type='import')
             if imports.exists():
-                try:
-                    import_code = imports[0].code
-
-                    exec(import_code, locals())
-                    exec(code, locals())
-                    dataframe = pd.read_csv(file_path)
-                    dataframe_unlabeled = dataframe.loc[(dataframe['splitting_id'] == 'unlabeled')]
-                    dataframe_train = dataframe.loc[(dataframe['splitting_id'] == 'train')]
-                    text_list_train_gold_labels = np.array(dataframe_train['CLASS'].tolist())
-                    local_var = locals()
-
-                    lfs = [local_var[name]]
-                    with queries_disabled():
-                        applier = PandasLFApplier(lfs=lfs)
-                        L_unlabeled = applier.apply(df=dataframe_unlabeled)
-                        L_train = applier.apply(df=dataframe_train)
-                    labelsummary_unlabeled = LFAnalysis(L=L_unlabeled, lfs=lfs).lf_summary()
-                    labelsummary_train = LFAnalysis(L=L_train, lfs=lfs).lf_summary(Y=text_list_train_gold_labels)
-                    labelsummary_unlabeled['index'] = labelsummary_unlabeled.index
-                    labelsummary_train = labelsummary_train.rename(columns={"Emp. Acc.": "EmpAcc"})
-                    labelsummary_train['index'] = labelsummary_train.index
-
-                    return status.HTTP_200_OK, {'summary': labelsummary_unlabeled, 'summary_train': labelsummary_train}
-                except:
-                    data = str(sys.exc_info())
-                    return status.HTTP_400_BAD_REQUEST, data
+                labels = Labelfunction.objects.filter(workflow_id=workflow_id, type='labels')
+                if labels.exists():
+                    labels_code = labels[0].code
+                    return self.__test_on_dataset(code, file_path, imports, name, workflow_id, labels_code)
+                return self.__test_on_dataset(code, file_path, imports, name, workflow_id)
             return status.HTTP_404_NOT_FOUND, {"message": "No import statements where found. from snorkel.labeling import labeling_function needs to be imported"}
         return status.HTTP_404_NOT_FOUND, {"message": "No data set has been uploaded"}
+
+    def __test_on_dataset(self, code, file_path, imports, name, workflow_id, labels_code=''):
+        try:
+            import_code = imports[0].code
+            exec(import_code, locals())
+            exec(labels_code, locals())
+            exec(code, locals())
+            dataframe = pd.read_csv(file_path)
+            dataframe_unlabeled = dataframe.loc[(dataframe['splitting_id'] == 'unlabeled')]
+            dataframe_train = dataframe.loc[(dataframe['splitting_id'] == 'train')]
+            text_list_train_gold_labels = np.array(dataframe_train['CLASS'].tolist())
+            local_var = locals()
+            lfs = [local_var[name]]
+            with queries_disabled():
+                applier = PandasLFApplier(lfs=lfs)
+                L_unlabeled = applier.apply(df=dataframe_unlabeled)
+                L_train = applier.apply(df=dataframe_train)
+            L_train_list = np.array(L_train).flatten().tolist()
+            L_unlabeled_list = np.array(L_unlabeled).flatten().tolist()
+            self.__checkLabels(workflow_id, L_train_list, L_unlabeled_list)
+            labelsummary_unlabeled = LFAnalysis(L=L_unlabeled, lfs=lfs).lf_summary()
+            labelsummary_train = LFAnalysis(L=L_train, lfs=lfs).lf_summary(Y=text_list_train_gold_labels)
+            labelsummary_unlabeled['index'] = labelsummary_unlabeled.index
+            labelsummary_train = labelsummary_train.rename(columns={"Emp. Acc.": "EmpAcc"})
+            labelsummary_train['index'] = labelsummary_train.index
+            return status.HTTP_200_OK, {'summary': labelsummary_unlabeled, 'summary_train': labelsummary_train}
+        except:
+            data = str(sys.exc_info())
+            return status.HTTP_400_BAD_REQUEST, data
 
     def get_all_labelfunction_by_workflow_id(self, workflow_id):
         labelfunction = Labelfunction.objects.filter(workflow_id=workflow_id).exclude(type='import').exclude(type='labels').order_by('creator')
--- snorkel_flow_backend/workflow_settings/services/run_service/run_service.py
+++ snorkel_flow_backend/workflow_settings/services/run_service/run_service.py
@@ -29,55 +29,66 @@ class RunService:
         if run_filter.exists():
             run_obeject = run_filter[0]
             run_labelfunctions = run_obeject.labelfunctions.all()
-            file = File.objects.filter(workflow_id=run_filter[0].workflow_id)
+            workflow_id = run_filter[0].workflow_id
+            file = File.objects.filter(workflow_id=workflow_id)
             if file.exists():
                 file_name = file[0].__str__()
                 file_path = "{root}/{name}".format(root=MEDIA_ROOT, name=file_name)
                 imports = Labelfunction.objects.filter(workflow_id=run_filter[0].workflow_id, type='import')
                 if imports.exists():
-                    try:
-                        exec(imports[0].code, locals())
-
-                        dataframe = pd.read_csv(file_path)
-
-                        dataframe_unlabeled = dataframe.loc[(dataframe['splitting_id'] == 'unlabeled')]
-                        dataframe_train = dataframe.loc[(dataframe['splitting_id'] == 'train')]
-                        text_list_train_gold_labels = np.array(dataframe_train['CLASS'].tolist())
-
-                        labelfunction_names = []
-                        for item in run_labelfunctions:
-                            exec(item.code, locals())
-                            labelfunction_names.append(item.name)
-                        local_vars = locals()
-
-                        labelfunction_reference = []
-                        for label in labelfunction_names:
-                            labelfunction_reference.append(local_vars[label])
-
-                        with queries_disabled():
-                            applier = PandasLFApplier(lfs=labelfunction_reference)
-                            L_train_unlabeled = applier.apply(df=dataframe_unlabeled)
-                            L_train_train = applier.apply(df=dataframe_train)
-
-                        summary = LFAnalysis(L=L_train_unlabeled, lfs=labelfunction_reference).lf_summary()
-                        summary_train = LFAnalysis(L=L_train_train, lfs=labelfunction_reference).lf_summary(Y=text_list_train_gold_labels)
-                        summary['index'] = summary.index
-                        summary_train = summary_train.rename(columns={"Emp. Acc.": "EmpAcc"})
-                        summary_train['index'] = summary_train.index
-
-                        data = {'summary': summary, 'summary_train': summary_train}
-                        labelmatrix = json.dumps(L_train_unlabeled.tolist())
-                        run_obeject.labelfunction_summary = summary.to_json(orient='split')
-                        run_obeject.labelmatrix = labelmatrix
-                        run_obeject.labelfunction_summary_train = summary_train.to_json(orient='split')
-                        run_obeject.save()
-                        return status.HTTP_200_OK, data
-                    except:
-                        data = str(sys.exc_info())
-                        return status.HTTP_400_BAD_REQUEST, data
+                    labels = Labelfunction.objects.filter(workflow_id=workflow_id, type='labels')
+                    if labels.exists():
+                        labels_code = labels[0].code
+                        return self.__execute_run_on_dataset(file_path, imports, run_labelfunctions,
+                                                             run_obeject, labels_code)
+                    return self.__execute_run_on_dataset(file_path, imports, run_labelfunctions,
+                                                         run_obeject)
         return status.HTTP_404_NOT_FOUND, {"message": "The run does not exist"}
 
-# todo get analysis wegmachen und direkt in exekute run machen
+    def __execute_run_on_dataset(self, file_path, imports, run_labelfunctions, run_obeject, labels_code=''):
+        try:
+            exec(imports[0].code, locals())
+            exec(labels_code, locals())
+            dataframe = pd.read_csv(file_path)
+
+            dataframe_unlabeled = dataframe.loc[(dataframe['splitting_id'] == 'unlabeled')]
+            dataframe_train = dataframe.loc[(dataframe['splitting_id'] == 'train')]
+            text_list_train_gold_labels = np.array(dataframe_train['CLASS'].tolist())
+
+            labelfunction_names = []
+            for item in run_labelfunctions:
+                exec(item.code, locals())
+                labelfunction_names.append(item.name)
+            local_vars = locals()
+
+            labelfunction_reference = []
+            for label in labelfunction_names:
+                labelfunction_reference.append(local_vars[label])
+
+            with queries_disabled():
+                applier = PandasLFApplier(lfs=labelfunction_reference)
+                L_train_unlabeled = applier.apply(df=dataframe_unlabeled)
+                L_train_train = applier.apply(df=dataframe_train)
+
+            summary = LFAnalysis(L=L_train_unlabeled, lfs=labelfunction_reference).lf_summary()
+            summary_train = LFAnalysis(L=L_train_train, lfs=labelfunction_reference).lf_summary(
+                Y=text_list_train_gold_labels)
+            summary['index'] = summary.index
+            summary_train = summary_train.rename(columns={"Emp. Acc.": "EmpAcc"})
+            summary_train['index'] = summary_train.index
+
+            data = {'summary': summary, 'summary_train': summary_train}
+            labelmatrix = json.dumps(L_train_unlabeled.tolist())
+            run_obeject.labelfunction_summary = summary.to_json(orient='split')
+            run_obeject.labelmatrix = labelmatrix
+            run_obeject.labelfunction_summary_train = summary_train.to_json(orient='split')
+            run_obeject.save()
+            return status.HTTP_200_OK, data
+        except:
+            data = str(sys.exc_info())
+            return status.HTTP_400_BAD_REQUEST, data
+
+    # todo get analysis wegmachen und direkt in exekute run machen
 #     def get_analysis(self, run_id):
 #         run = Run.objects.filter(pk=run_id)
 #         if run.exists():
