34dc277676b30e5184a110b6581e437777de18f8
mariebraun <marie.braun@uni-duesseldorf.de>
2024-06-01 02:13:30 +0200

    add tutorial about Labelfunktions

--- snorkel-flow-frontend/src/app/components/support/tutorial/labelfunction-tutorial/labelfunction-tutorial.component.ts
+++ snorkel-flow-frontend/src/app/components/support/tutorial/labelfunction-tutorial/labelfunction-tutorial.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-labelfunction-tutorial',
   templateUrl: './labelfunction-tutorial.component.html',
-  styleUrls: ['./labelfunction-tutorial.component.css']
+  styleUrls: ['./labelfunction-tutorial.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LabelfunctionTutorialComponent {
   visible: boolean = false;
