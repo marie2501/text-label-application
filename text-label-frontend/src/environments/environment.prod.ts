6ad75011c0f1b738432c3590b83b64943c7c3b30
mariebraun <marie.braun@uni-duesseldorf.de>
2025-01-06 02:38:34 +0100

    Frontend: update env.prod

--- text-label-frontend/src/environments/environment.prod.ts
+++ text-label-frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 export const environmentProd = {
   // ip_adresse: process.env.IPADRESSE,
   // port: process.env.PORT,
-  ip_adresse: 'textlabel.everythinkatonce.de',
+  ip_adresse: 'localhost',
   port: 80,
   protocol: 'https',
 };
